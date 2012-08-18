615bfcd7da5b0240eb286843fddf52648572dab7
Sergey Generalov <sergey.genbit@gmail.com>
2012-08-17 17:44:12 -0700

    add frontend application, basic templates and bootstrap

--- django_tutorial/django_tutorial/settings.py
+++ django_tutorial/django_tutorial/settings.py
@@ -1,5 +1,14 @@
 # Django settings for django_tutorial project.
 
+import os
+
+# path to the project dir
+PROJECT_DIR = os.path.abspath(os.path.dirname(__file__))
+
+# helper function to construct project related path
+project_path = lambda x: os.path.realpath(os.path.join(PROJECT_DIR, x))
+
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -56,7 +65,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = project_path('static')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -115,6 +124,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'frontend'
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
--- django_tutorial/frontend/urls.py
+++ django_tutorial/frontend/urls.py
@@ -1,11 +1,9 @@
 from django.conf.urls import patterns, include, url
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from views import *
 
 urlpatterns = patterns('',
-    url(r'', include('frontend.urls')),
+    url(r'', index, name='index'),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
