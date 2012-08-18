279876fa8066d2086dc6b6bf670a8e49609dfd04
Ivan Vershigora <limpbrains@mail.ru>
2012-08-18 13:34:14 +0400

    add local_settings

--- django_tutorial/settings.py
+++ django_tutorial/settings.py
@@ -1,6 +1,7 @@
 # Django settings for django_tutorial project.
 
 import os
+import dj_database_url
 
 # path to the project dir
 PROJECT_DIR = os.path.abspath(os.path.dirname(__file__))
@@ -18,16 +19,7 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'devel.db',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
-}
+DATABASES = {'default': dj_database_url.config(default='postgres://localhost')}
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -160,3 +152,8 @@ LOGGING = {
         },
     }
 }
+
+try:
+    from local_settings import *
+except:
+    pass
